35c1e9ae6564b79f59f5c4343322119284e69f61
Marcos Vinicius <marcos.carini22@gmail.com>
2025-03-06 03:13:58 -0300

    feat: add new button on page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,8 @@ const ProductPage = () => {
   return (
     <div className="rounded-xl border border-red-500 p-5">
       <h1 className="text-red-500">product page</h1>
-      <Button>FSW 7.0</Button>
+      <Button>FSW 7.1</Button>
+      <Button>Oi</Button>
       <Input placeholder="Bora fechar esse projeto!" />
     </div>
   );
